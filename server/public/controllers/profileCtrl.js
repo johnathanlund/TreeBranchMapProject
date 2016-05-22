df1542a0944d41f3911721198b846a1c3fda1117
Johnathan Lund <Johnathan Lund>
2016-05-22 01:04:25 -0600

    css update, working on mobile menu

--- server/public/controllers/profileCtrl.js
+++ server/public/controllers/profileCtrl.js
@@ -71,7 +71,7 @@ $scope.markerDelete = function (marker) {
 
 // This controls the group lists, so that when you click on a group tab, only
 // that group div will show, and not all group divs. This took hours of trial &
-// error, research & consulting to make seperate group divs, with matching _id from 
+// error, research & consulting to make seperate group divs, with matching _id from
 // ng-repeat, work with just their group._id and not show all groups in the repeat.
 $scope.select = function (group) {
   angular.forEach($scope.groups, function (currentGroup) {
@@ -79,6 +79,16 @@ $scope.select = function (group) {
   });
 };
 
+var lastScrollTop = 0;
+$(document).on('scroll', function () {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+        $('.navBar').slideUp(500);
+    } else {
+        $('.navBar').slideDown(500);
+    }
+    lastScrollTop = st;
+});
 
 //     $scope.groupTabs = false;
 //     $scope.groupToggle = function() {
--- server/public/directive.js
+++ server/public/directive.js
@@ -11,6 +11,17 @@ angular.module('mapFavApp')
     restrict: "AE",
     templateUrl: './templates/navMenu.html',
     controller: function () {
+      var lastScrollTop = 0;
+      $(document).on('scroll', function () {
+          var st = $(this).scrollTop();
+          if (st > lastScrollTop) {
+              $('.navBar-container').slideUp(500);
+          } else {
+              $('.navBar-container').slideDown(500);
+          }
+          lastScrollTop = st;
+      });
+
       $('#menu-icon').on('click', function () {
         $('#nav-menu').toggle({
           duration: 1500,
